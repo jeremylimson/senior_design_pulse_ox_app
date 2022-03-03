6d52e6126db7ea09cd401db22533db586d31fda6
Jeremy Limson <63616127+jeremylimson@users.noreply.github.com>
2022-03-03 16:01:07 -0600

    added trends page and circle widget

--- RoomWordSample/app/src/main/java/com/example/roomwordsample/MainActivity.java
+++ RoomWordSample/app/src/main/java/com/example/roomwordsample/MainActivity.java
@@ -7,13 +7,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int saturation = 0;
+    private ProgressBar progressBar;
+    private Button startProgress;
 
     private WordViewModel mWordViewModel;
     public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
@@ -34,16 +41,45 @@ public class MainActivity extends AppCompatActivity {
             adapter.submitList(words);
         });
 
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
-        fab.setOnClickListener( view -> {
+        FloatingActionButton word = findViewById(R.id.floatingActionButton);
+        word.setOnClickListener( view -> {
             Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
             startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
         });
 
-        FloatingActionButton fab2 = findViewById(R.id.floatingActionButton2);
-        fab2.setOnClickListener( view -> {
+        FloatingActionButton setting = findViewById(R.id.floatingActionButton2);
+        setting.setOnClickListener( view -> {
             Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+        });
+
+        FloatingActionButton trends = findViewById(R.id.floatingActionButton3);
+        trends.setOnClickListener( view -> {
+            Intent intent = new Intent(MainActivity.this, TrendsActivity.class);
+            startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+        });
+
+        progressBar = findViewById(R.id.progress_bar);
+        startProgress = findViewById(R.id.startProgress);
+
+        CountDownTimer countDownTimer = new CountDownTimer(11*1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                saturation += 10;
+                progressBar.setProgress(saturation);
+                progressBar.setMax(100);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        startProgress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.start();
+            }
         });
     }
 
